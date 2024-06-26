67230edf0fa7b83935e03bf7c6bca154d231956d
David S <z5310968@ad.unsw.edu.au>
2024-06-26 14:26:47 +1000

    working on WatchList modal

--- frontend1/src/company/WatchlistModal.jsx
+++ frontend1/src/company/WatchlistModal.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from '@mui/material/Modal';
+import { Button, Typography } from "@mui/material";
 
-const WatchlistModal = ({ open }) => {
+const WatchlistModal = ({ isOpen }) => {
+    const [watchlist, setWatchist] = useState([]); //this should be populated with whatever lists the user has 
+    const [listName, setListName] = useState(false);
 
+    const handleListName = () => {
+        setListName(true);
+    }
+
+    return (
+        <Modal 
+            open = {isOpen}>
+            <Typography variant="body2">Add to watchlist</Typography>
+            <div>
+                {listName ?
+                (<Box sx={{ display: 'flex', alignItems: 'center'}}><TextField fullWidth id="standard-basic" label="new watchlist name" variant="standard"/><Button>OK</Button></Box>) 
+                : (<Button onClick={handleListName}>+ New WatchList</Button>)
+                };
+            </div>
+        </Modal>
+    );
 };
 
 export default WatchlistModal;
