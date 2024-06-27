052a343195e0908c257d8799be527cb407ec942a
mariali383 <mli.maria383@gmail.com>
2024-06-27 12:37:20 +1000

    User route schema update

--- backend/schemas.py
+++ backend/schemas.py
@@ -7,7 +7,7 @@ class Token(BaseModel):
     token_type: str # honestly might not need this?
 
 class TokenData(BaseModel):
-    username: Union[str, None] = None
+    userId: Union[str, None] = None
 
 class User(BaseModel):
     id: int
@@ -22,9 +22,6 @@ class UserRegister(BaseModel):
     password: Union[str, None] = None
     
 class UserInDB(User):
-    # id: int
-    # email: Union[str, None] = None
-    # full_name: Union[str, None] = None
     hashed_password: str
     
     
