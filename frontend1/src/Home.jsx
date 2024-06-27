0a6639a2aff430a457cb4e36d441b99f95d4b58a
Gavin Li <gavin5325@gmail.com>
2024-06-27 18:54:22 +1000

    added a learn more button

--- frontend1/src/Home.jsx
+++ frontend1/src/Home.jsx
@@ -81,6 +81,7 @@ const Home = () => {
           <Button className="titleButton registerButton" type="submit" variant="contained" onClick={openRegisterModal}>Register</Button>
         </div>
       </div>
+      <button id="learnMore">learn more</button>
       </div>
       <div id='homeAbout'>
         <div className='aboutContainer'>
@@ -106,7 +107,6 @@ const Home = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
-
       </div>
     </div>
 	);
