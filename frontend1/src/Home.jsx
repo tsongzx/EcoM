dd0b7c4325f4c41ce29a5c7d1d2539492727e08c
David S <z5310968@ad.unsw.edu.au>
2024-06-29 02:10:24 +1000

    added scroll button in home page

--- frontend1/src/Home.jsx
+++ frontend1/src/Home.jsx
@@ -81,7 +81,19 @@ const Home = () => {
           <Button className="titleButton registerButton" type="submit" variant="contained" onClick={openRegisterModal}>Register</Button>
         </div>
       </div>
-      <button id="learnMore">learn more</button>
+      <button id="learnMore" onClick={() => {
+        window.scrollBy({
+          top:  window.innerHeight,
+          behavior: "smooth",
+        });
+      }}>
+      <svg fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330">
+        <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+          c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+          s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/>
+      </svg>
+        learn more
+      </button>
       </div>
       <div id='homeAbout'>
         <div className='aboutContainer'>
@@ -107,6 +119,15 @@ const Home = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
+
+        <div className='aboutContainer'>
+          <div className='aboutTextContainer'>
+            <h2>Comparison mode</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <img src = {imageCat} alt='placeholderImage'/>
+        </div>
+
       </div>
     </div>
 	);
