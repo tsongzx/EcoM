a950a12b279ce510c98f833102f4aa348b268ff8
mariali383 <mli.maria383@gmail.com>
2024-07-07 20:49:38 +1000

    changed db

--- backend/import_company_data.sh
+++ backend/import_company_data.sh
@@ -2,11 +2,17 @@
 # change path to where you put your ESG data
 cd "C:/Users/byunt/OneDrive - UNSW/Documents/UNSW/2024/T2/COMP3900/ESG Data"
 MYSQL_OUTPUT="mysql_output.log"
+host='crumpeteers.chg60uma2isb.ap-southeast-2.rds.amazonaws.com'
+username='admin'
+password='crumpeteers'
+db='crumpeteers'
+
+# below is required when importing to local db
+# SET GLOBAL local_infile=1;
 
 for f in *.csv
 do
-mysql --local-infile=1 -t -u root --password=password -t crumpeteers << EOF | tee -a $MYSQL_OUTPUT
-  SET GLOBAL local_infile=1;
+mysql --local-infile=1 -t -h $host -u $username --password=$password -t $db << EOF | tee -a $MYSQL_OUTPUT
   LOAD DATA LOCAL INFILE '$f'
     INTO TABLE CompanyList 
     FIELDS TERMINATED BY '|' 
