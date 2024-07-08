fa1a73c46c6243edb72cdb330b4bbade52a23d6d
David S <z5310968@ad.unsw.edu.au>
2024-07-09 00:09:33 +1000

    started implementing dashboard element to take you to watchlist

---
+++ frontend1/src/ListElement.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * 
+ * @param {*} param0 gets passed in the id of the LIST, fetches the information on the list
+ * @returns 
+ */
+const ListElement = ({id}) => {
+
+    return (
+        <div className="listElement">
+
+        </div>
+    );
+}
+
+export default ListElement;
