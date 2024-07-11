8d49666249c053fed5f5ea9203fb6cbad57292f8
David S <z5310968@ad.unsw.edu.au>
2024-07-11 14:35:59 +1000

    More profile implementation

--- frontend1/src/Navbar.jsx
+++ frontend1/src/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 import Cookies from 'js-cookie';
+import Profile from './Profile';
 
 const Navbar = () => {
     const navigate = useNavigate();
-
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
     // Go back to the first page
     const handleLogout = () => {
         Cookies.remove('authToken');
@@ -26,6 +27,8 @@ const Navbar = () => {
                 <li><a href="/dashboard" onClick={handleHome}>Home</a></li>
                 <li><a href="/" onClick={handleLogout}>Logout</a></li>
             </ul>
+            <button onClick={() => {setIsProfileOpen(!isProfileOpen)}}>Profile</button>
+            {isProfileOpen && (<Profile/>)}
         </div>
     );
 }
--- frontend1/src/Profile.jsx
+++ frontend1/src/Profile.jsx
@@ -10,18 +10,87 @@ const Profile = () => {
     const [password, setPassword] = useState('');
     //updated versions are just to keep the old passwords
     const [updatedPassword, setUpdatedPassword] = useState('');
+    const [updatedConfirmPassword, setUpdatedConfirmPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     // 0) It could be passed in a URL such that the return key 
     // will navigate the user back to where they left off
 
     // 1) get User and Display data
 
     // 2) Update the User's password (include confirm password)
+    const handleUpdatePassword = () => {
+        //check password is different to the old one
+        if (updatedPassword === password) {
+            setErrorMessage('Cannot use previous password');
+            setUpdatedPassword('');
+            setUpdatedConfirmPassword('');
+            return;
+        }
+        if (!(updatedPassword === updatedConfirmPassword)) {
+            setErrorMessage('Passwords do not match');
+            setUpdatedPassword('');
+            setUpdatedConfirmPassword('');
+            return;
+        }
+        if (updatedPassword.length < 8) {
+            setErrorMessage('Password has to be greater than 8 characters');
+            setUpdatedPassword('');
+            setUpdatedConfirmPassword('');
+            return;
+        }
+        //push changes to backend
+        updatePassword(password, updatedPassword, updatedConfirmPassword);
+        //reset password values
+        setPassword(updatedPassword);
+        setUpdatedPassword('');
+        setUpdatedConfirmPassword('');
+        showUpdatePassword(false);
+    }
+
+    const updatePassword = async (old, newP, newConfirmed) => {
+        try {
+            const response = await axios.put('http://127.0.0.1:8000/user/full-name', {
+                old_password: old,
+                new_password: newP,
+                confirm_password: newConfirmed
+            }, {
+                header: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            setErrorMessage('There was an oopsie:', error);
+        }
+    }
 
     // 3) Update the User's name
     const handleCloseUpdateName = () => {
+        if (name === updatedName) {
+            return;
+        }
         setShowUpdateName(false);
         setName(updatedName);
+
         //push changes to backend
+        updateName(name);
+    }
+
+    const updateName = async (name) => {
+        try {
+            const response = await axios.put('http://127.0.0.1:8000/user/full-name', {
+                new_name: name
+            }, {
+                header: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // 4) When the user presses enter or esc, it exits a text box or submits
@@ -38,6 +107,20 @@ const Profile = () => {
                     <p>{name}</p>
                     <button onClick={() => {setShowUpdateName(true)}}>edit</button>
                 </div>)}
+                {showUpdatePassword 
+                ? (<div>
+                    <input type="password" onChange={(event) => {setUpdatedPassword(event.target.value)}}/>
+                    <input type="password" onChange={(event) => {setUpdatedConfirmPassword(event.target.value)}}/>
+                    <button onClick={handleUpdatePassword}>OK</button>
+                </div>) 
+                : (<div>
+                    <input type={password} defaultValue={password} readOnly/>
+                    <button onClick={() => {setShowUpdatePassword(true)}}>edit</button>
+                </div>)}
+                <p>{errorMessage}</p>
+                <div className="profilePageLists">
+
+                </div>
         </div>
     )
 }
