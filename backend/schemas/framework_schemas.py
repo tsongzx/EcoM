cc98b9811f95424c6d8e02dade0880c55da743f6
mariali383 <mli.maria383@gmail.com>
2024-07-17 17:24:39 +1000

    added customise framework apis

---
+++ backend/schemas/framework_schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+# from typing import Union
+# from sqlalchemy import DateTime
+import datetime
+
+class CustomFramework(BaseModel):
+    framework_name: str
+    description: str
+    user_id: int
+    
+class CustomFrameworkMetrics(BaseModel):
+    parent_id: int
+    metric_id: int
+    weighting: float
+
+class Framework(BaseModel):
+    is_official_framework: bool
+    framework_id: int
+    
