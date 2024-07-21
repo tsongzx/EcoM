3442e38aab875b1ae77552e8978a55c1a545429f
David S <z5310968@ad.unsw.edu.au>
2024-07-21 14:26:47 +1000

    Implemented Creating a New framework

--- frontend1/src/company/CreateFramework.jsx
+++ frontend1/src/company/CreateFramework.jsx
@@ -3,13 +3,15 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import './CreateFramework.css'
 import { TextField } from "@mui/material";
-import { getUserId, getMetricName } from "../helper.js";
+import { getUserId } from "../helper.js";
+import Textarea from '@mui/joy/Textarea';
 
 const CreateFramework = () => {
     const [metrics, setMetrics] = useState([]);
     const [activeMetrics, setActiveMetrics] = useState([]);
     const [showAddName, setShowAddName] = useState(false);
     const [newFrameworkName, setNewFrameworkName] = useState('');
+    const [description, setDescription] = useState('');
 
     useEffect(() => {
         const fetchMetrics = async () => {
@@ -26,7 +28,7 @@ const CreateFramework = () => {
                 console.error('Failed to fetch metrics:', error);
             }
         };
-
+        console.log('DOING STUFF FOR CREATEFRAMEWORK...');
         fetchMetrics();
     }, []);
 
@@ -49,41 +51,51 @@ const CreateFramework = () => {
 
     //toggle the state of a button to be checked
     const handleButtonClick = (index) => {
-        setMetrics(prevMetrics => {
-            const newMetrics = [...prevMetrics];
-            newMetrics[index].isIncluded = !newMetrics[index].isIncluded; // Toggle the isIncluded property
-            return newMetrics;
-        });
+        console.log('OLD METRICS:', metrics[index]);
+        const newMetrics = [...metrics];
+        newMetrics[index].isIncluded = !newMetrics[index].isIncluded;
+        setMetrics(newMetrics);
     }
 
     const handleSubmitNewFramework = async () => {
     const filteredMetrics = metrics.filter(metric => metric.isIncluded)
-    const ChosenMetrics = filteredMetrics.map(metric => ({
-        parent_id: -1,
+    // const weighting = 100 / filteredMetrics.length
+    const chosenMetrics = filteredMetrics.map(metric => ({
+        parent_id: 1,
         metric_id: metric.id,
-        weighting: 0
+        weighting: 0.0
     }));
-
+    const userInfo = await getUserId();
+    console.log(userInfo);
+    console.log(`Submitting Framework: ${newFrameworkName} with metrics:`);
+    console.log(chosenMetrics);
         try {
-            const response = await axios.post(`http://127.0.0.1:8000/framework/create`,
+            const response = await axios.post(`http://127.0.0.1:8000/framework/create/`,
                 {
                     details: {
                         framework_name: newFrameworkName,
-                        description: "",
-                        user_id: await getUserId()
+                        description: description,
+                        user_id: userInfo.id
                     },
-                    metrics: ChosenMetrics,
+                    metrics: chosenMetrics,
                 },
                 {headers: {
                   'Content-Type': 'application/json',
                   'Authorization': `Bearer ${Cookies.get('authToken')}`
               }});
               //if successful
-              console.log('SUCCESSFULLY FETCHED METRICS');
+              handleClose();
+              console.log('SUCCESSFULLY CREATED FRAMEWORK METRICS');
               return response.data;
         } catch (error) {
             console.log(error);
         }
+    };
+
+    const handleClose = () => {
+        setShowAddName(false);
+        setNewFrameworkName('');
+        setDescription('');
     }
 
     return (
@@ -97,8 +109,9 @@ const CreateFramework = () => {
             ))}
             <button onClick={() => setShowAddName(!showAddName)}>Create Framework</button>
             {showAddName && (<div>
-                <button onClick={() => setShowAddName(false)}>X</button>
-                    <TextField onChange={(event) => setNewFrameworkName(event.target.value)} label="Framework Name" variant="standard" />
+                <button onClick={handleClose}>X</button>
+                    <TextField onChange={(event) => setNewFrameworkName(event.target.value)} label="Framework Name" variant="standard"/>
+                    <Textarea minRows={3} size="sm" placeholder="Describe your Framework!" onChange={(event) => setDescription(event.target.value)}/>
                 <button onClick={() => handleSubmitNewFramework()}>save</button>
             </div>)}
         </div>
