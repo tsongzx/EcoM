5a600eb1f51d37fa7fe1f65264283ed1f3621894
mariali383 <mli.maria383@gmail.com>
2024-07-21 19:11:44 +1000

    added category enum to framework models

--- backend/models/framework_models.py
+++ backend/models/framework_models.py
@@ -3,29 +3,45 @@ from sqlalchemy import String, DateTime, Enum
 from db import Base
 import datetime
 from datetime import timezone
+from typing import Literal, get_args
 
-class OfficialFrameworks(Base):
-    __tablename__ = 'OfficialFrameworks'
+Category = Literal["E", "S", "G"]
+
+# class OfficialFrameworks(Base):
+#     __tablename__ = 'OfficialFrameworks'
+    
+#     id: Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
+#     framework_name: Mapped[str] = mapped_column(String(100), nullable=False)
+#     description: Mapped[str] = mapped_column(String(1000), nullable=False)
+
+# class Categories(Base):
+#     id: Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
+#     category: Mapped[str] = mapped_column(String(100), nullable=False)
+    
+class Frameworks(Base):
+    __tablename__ = 'Frameworks'
     
     id: Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
     framework_name: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str] = mapped_column(String(1000), nullable=False)
-
-class UserFrameworks(Base):
-    __tablename__ = 'UserFrameworks'
-    
-    id: Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
-    framework_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    description: Mapped[str] = mapped_column(String(1000), nullable=False)
-    user_id: Mapped[int] = mapped_column(unique=False, nullable=False)
+    user_id: Mapped[int] = mapped_column(unique=False)
+    is_official_framework: Mapped[bool]= mapped_column(primary_key=False, unique=False, nullable=False)
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.datetime.now(timezone.utc))
-    
+    E: Mapped[float] = mapped_column(unique=False)
+    S: Mapped[float] = mapped_column(unique=False)
+    G: Mapped[float] = mapped_column(unique=False)
+
 class OfficialFrameworkMetrics(Base):
     __tablename__ = 'OfficialFrameworkMetrics'
     
     id: Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
     framework_id: Mapped[int] = mapped_column(primary_key=False, unique=False, nullable=False)
-    parent_id: Mapped[int] = mapped_column(primary_key=False, unique=False, nullable=False)
+    category: Mapped[Category] = mapped_column(Enum(
+      *get_args(Category),
+      name="category",
+      create_constraint=True,
+      validate_strings=True,
+    ))
     metric_id: Mapped[int] = mapped_column(primary_key=False, unique=False, nullable=False)
     # to add
     # default_weight: Mapped[float] = mapped_column(primary_key=False, unique=False)
@@ -34,10 +50,13 @@ class CustomMetrics(Base):
     __tablename__ = 'CustomMetrics'
     
     id: Mapped[int]= mapped_column(primary_key=True, unique=True, nullable=False)
-    is_official_framework: Mapped[bool]= mapped_column(primary_key=False, unique=False, nullable=False)
     framework_id: Mapped[int] = mapped_column(primary_key=False, unique=False, nullable=False)
-    parent_id: Mapped[int] = mapped_column(primary_key=False, unique=False, nullable=False)
-    # subcategory: Mapped[str] = mapped_column(String(100), nullable=False)
+    category: Mapped[Category] = mapped_column(Enum(
+      *get_args(Category),
+      name="category",
+      create_constraint=True,
+      validate_strings=True,
+    ))
     metric_id: Mapped[int] = mapped_column(primary_key=False, unique=False, nullable=False)
     weighting: Mapped[float] = mapped_column(primary_key=False, unique=False, nullable=False)
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.datetime.now(timezone.utc))
