31a06450c3de13d7d8217a3829094addc0ff1745
mariali383 <mli.maria383@gmail.com>
2024-07-30 14:47:31 +1000

    added indicator visualisations

--- frontend1/src/company/Visualisations.jsx
+++ frontend1/src/company/Visualisations.jsx
@@ -1,82 +1,76 @@
 import React, { useEffect, useState } from 'react';
-import BarChartStyled from './visualisations/BarChartStyled';
-import { v4 as uuidv4 } from 'uuid';
-import Card from '@mui/material/Card';
-
+import { getAllIndicators } from '../helper';
+import VisualisationsTab from './visualisations/VisualisationsTab';
+import { Box } from '@mui/material';
 
 const Visualisations = ({companyIndicators, companyName}) => {
   // indicator visualisations
-  // get all indicators 
-  // add panel to remove / select indicators? - not important
   const [graphValues, setGraphValues] = useState({});
+
   const [selectedYears, setSelectedYears] = useState([]);  
+  const [indicatorInfo, setIndicatorInfo] = useState({});
+    
+  useEffect(() => {
+    const getIndicatorInfo = async() => {
+      const info = await getAllIndicators();
+      console.log(info);
+      setIndicatorInfo(info);
+    }
+    getIndicatorInfo();
+  }, []);
+
   // create graph for each indicator
   // group indicators with same unit together - plot together
   
   const get_graph_values = () => {
-    const graph_data = {}
-    console.log(companyIndicators);
-    for (const [year, data] of Object.entries(companyIndicators)) {
+    const graph_data = {};
+    for (const [year, data] of Object.entries(companyIndicators)) {  
       for (const [indicator_name, indicator_data] of Object.entries(data)) {
-        // check if within year
-        if (!(indicator_data.indicator_year_int in selectedYears)) {
+        // Check if within selected years
+        if (!selectedYears.includes(indicator_data.indicator_year_int.toString())) {
+          // console.log(`Skipping year ${indicator_data.indicator_year_int} as it is not in selectedYears`);
           continue;
         }
+  
         if (!(indicator_name in graph_data)) {
-          graph_data[indicator_name] = []
+          graph_data[indicator_name] = [];
         }
-        graph_data[indicator_name].push(
-          {
-            indicator: indicator_name,
-            year: year,
-            value: indicator_data.indicator_value,
-            company: companyName
-          }
-        )
+  
+        graph_data[indicator_name].push({
+          indicator: indicator_name,
+          year: year,
+          value: indicator_data.indicator_value,
+          company: companyName,
+        });
       }
     }
-    console.log(graph_data);
+    console.log('graph_data:', graph_data);
     return graph_data;
-  }
-
-  // const get_graph_values = () => {
-  //   graph_data_by_ind = {};  
-  //   for (const [year, data] of Object.entries(object)) {
-  //     graph_data_by_year[year] = []
-  //     for (const entry of data) {
-  //       graph_data_by_year[year].push({
-  //         indicator: entry.indictor_name,
-  //         year: year,
-  //         value: data.indicator_value,
-  //         company: companyName
-  //       })
-  //     }
-  //   }
-  //   return graph_data_by_year;
-  // }
+  };
 
   useEffect(() => {
-    setGraphValues(get_graph_values(companyIndicators));
-    // right now, use all years
     setSelectedYears(Object.keys(companyIndicators));
   }, [companyIndicators]);
+  useEffect(() => {
+    console.log(Object.keys(companyIndicators));
+    setGraphValues(get_graph_values(companyIndicators));
+    // right now, use all years
+    console.log(selectedYears);
+  }, [companyIndicators, selectedYears]);
   
-  
-  // later user can toggle by year
-  // for now plot all 
+  const isDataReady = () => {
+    // Ensure both graphValues and indicatorInfo are objects and have data
+    return (
+      graphValues &&
+      indicatorInfo &&
+      Object.keys(graphValues).length > 0 &&
+      Object.keys(indicatorInfo).length > 0
+    );
+  };
   return (
     <>
-      {/* {graphValues ? <BarChartStyled data={graphValues['HUMAN_RIGHTS_VIOLATION_PAI']}/> : <p>Loading...</p>} */}
-
-
-      {Object.keys(graphValues).map((indicator) => {
-        return graphValues[indicator] ? (
-          <Card key={uuidv4()}>
-            <BarChartStyled data={graphValues[indicator]} />
-          </Card>
-        ) : null;
-      })}
-
+      { isDataReady() ?
+      (<VisualisationsTab indicatorInfo={indicatorInfo} graphValues={graphValues}></VisualisationsTab>) : (<Box>...loading</Box>)}
     </>
   );
 }
