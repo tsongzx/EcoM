a9fc0021e1f8c56ef216a74f32aa731c108e4ccb
Gavin Li <gavin5325@gmail.com>
2024-08-07 21:10:15 +1000

    Fixed bug on create open framework

--- backend/metrics.py
+++ backend/metrics.py
@@ -2,7 +2,8 @@ import functools
 import json 
 @functools.lru_cache
 def read_metrics_file():
-  file_name = 'backend/db/metrics.json'
+#   file_name = 'backend/db/metrics.json'
+  file_name = 'db/metrics.json'
   # Open and read the JSON file
   with open(file_name, 'r') as file:
       return json.load(file)
